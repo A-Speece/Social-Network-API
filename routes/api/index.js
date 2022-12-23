77349f0612bddfd242cd2fd01348eef8f6974938
Anthony Speece <anthonyspeece@gmail.com>
2022-12-23 13:56:46 -0600

    adding the userRoute and userController

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoute");
+const thoughtRoutes = require("./thoughtRoute");
 
 router.use("/User", userRoutes);
+router.use("/Thought", thoughtRoutes);
 
 module.exports = router;
--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
-const User = require("../../models/User");
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+} = require("../../controllers/userController");
 
-router.get("/", (req, res) => {
-  User.find({}).then((result) => {
-    res.json(results);
-  });
-});
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getSingleUser);
 
 module.exports = router;
