0839174b8430e65ef8b423d4f712a05e3127cd36
Anthony Speece <anthonyspeece@gmail.com>
2022-12-27 21:16:39 -0600

    adding update and delete user

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -3,6 +3,8 @@ const {
   getUsers,
   getSingleUser,
   createUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/userController");
 
 // /api/users
@@ -10,5 +12,8 @@ router.route("/").get(getUsers).post(createUser);
 
 // /api/users/:userId
 router.route("/:userId").get(getSingleUser);
+
+// /api/thought/:thoughtId
+router.route("/:usertId").put(updateUser).delete(deleteUser);
 
 module.exports = router;
